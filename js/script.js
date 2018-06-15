9181f270ec603f1614431b31591018cc93ba33cf
Wycliffe Momanyi <wickymoms@gmail.com>
2018-06-15 12:41:37 +0300

    implementation of google search

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function validate() {
+    var search=document.getElementById('search-box').value;
+    if(search==''){
+        alert('Opps you have not type anything please type something');
+    }else {
+        alert(search);
+    }
+}
+
+
